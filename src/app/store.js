b5acec52df9c5214cb6f2191585c1d885a53a8ad
Akash Rajput <akashrajput@Akashs-MacBook-Air.local>
2024-09-29 00:54:14 +0530

    Refactored store.js: Removed unused faceSlice import

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import faceReducer from '../features/faceDetection/faceSlice';
 import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
-    faceDetection: faceReducer,
     auth: authReducer,
   },
 });
