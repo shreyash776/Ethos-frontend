df7ae2139c87527f8119eb874eca65618b09014e
r3tr056 <dangerankur56@gmail.com>
2024-11-30 21:10:17 +0530

    Router in progress.

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,45 +1,43 @@
 import { Route, Routes } from 'react-router-dom';
-import AuthLayout from '../layouts/AuthLayout';
-import MainLayout from '../layouts/MainLayout';
-import SignIn from '../pages/Auth/SignIn.jsx';
-import SignUp from '../pages/Auth/SignUp.jsx';
-import Dashboard from '../pages/Dashboard/dashboard.jsx';
-import NotFoundPage from '../pages/NotFound/NotFoundPage.jsx';
-import UploadPage from '../pages/Upload/uploadPage.jsx';
+// import AuthLayout from '../layouts/AuthLayout.jsx';
+// import MainLayout from '../layouts/MainLayout.jsx';
+// import SignIn from '../pages/Auth/SignIn.jsx';
+// import SignUp from '../pages/Auth/SignUp.jsx';
+// import Dashboard from '../pages/Dashboard/dashboard.jsx';
+// import ProjectsPage from '../pages/Dashboard/projects.jsx';
+import NotFound from '../pages/NotFound/NotFoundPage.jsx';
+// import UploadPage from '../pages/Upload/uploadPage.jsx';
+import LandingPage from '../pages/Website/landing.jsx';
+import JoinWaitlist from '../pages/Website/waitlist_form.jsx';
 
 const Router = () => {
   const isAuthenticated = true;
 
   return (
     <Routes>
-      {/* Public routes */}
-      <Route element={<AuthLayout />}>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Route>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/waitlist" element={<JoinWaitlist />} />
+      {/* {!isAuthenticated ? (
+        <Route element={<AuthLayout />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      ) : (
+        <>
+          <Route element={<MainLayout />}>
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route
+              path="/process"
+              element={<Dashboard />}
+            />
 
-      {/* Protected routes */}
-      {/* <Route element={<MainLayout />}>
-        <Route
-          path="/"
-          element={isAuthenticated ? <Dashboard /> : <Navigate to="/signin" />}
-        /> */}
-      <Route element={<MainLayout />}>
-        <Route
-          path="/"
-          element={<Dashboard />}
-        />
-        <Route
-          path="/upload"
-          element={<UploadPage />}
-        />
+          </Route>
+        </>
+      )} */}
 
-
-      </Route>
-
-      {/* 404 Not Found */}
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+      <Route path="*" element={<NotFound />} />
+    </Routes >
   );
 };
 
