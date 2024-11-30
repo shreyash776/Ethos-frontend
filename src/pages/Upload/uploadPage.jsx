5bd5671cb1e9b08a32eb995dca257791c82b8b14
r3tr056 <dangerankur56@gmail.com>
2024-11-30 21:08:00 +0530

    Video Upload Page

--- src/pages/Upload/uploadPage.jsx
+++ src/pages/Upload/uploadPage.jsx
@@ -1,12 +1,24 @@
-import UploadVideo from "../../components/UploadVideo";
+import UploadVideo from '../../components/UploadVideo';
 
 const UploadPage = () => {
-
 	return (
-		<div className="flex flex-col items-center justify-center bg-gray-900 p-6 h-screen">
-			<UploadVideo />
+		<div className="h-full bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+			<div className="max-w-3xl mx-auto">
+				<div className="text-center mb-8">
+					<h1 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+						Upload a Video
+					</h1>
+					<p className="mt-4 text-xl text-gray-600">
+						Upload a video to see how our Face Reconstruction works
+					</p>
+				</div>
+				<div className="bg-white shadow-md rounded-lg overflow-hidden">
+					<UploadVideo />
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default UploadPage;
+
