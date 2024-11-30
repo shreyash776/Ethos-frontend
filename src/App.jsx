5b4709b126ba16e172fe584e70a146c0144187a0
r3tr056 <dangerankur56@gmail.com>
2024-11-30 21:10:45 +0530

    Extra bits. Here and there. Firebase and other things 👍

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import LoadingScreen from './components/LoadingScreen';
 import Router from './router/index';
 
+
 const App = () => {
+  const { isLoading } = useSelector((state) => state.loading);
+
   return (
     <div>
-      <Router />
-      <ToastContainer
-        position="top-right" 
-        autoClose={5000} 
-        hideProgressBar={false} 
-        newestOnTop={false} 
-        closeOnClick 
-        rtl={false} 
-        pauseOnFocusLoss 
-        draggable 
-        pauseOnHover
-      />
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <div>
+          <Router />
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      )
+      }
     </div>
   );
 };
